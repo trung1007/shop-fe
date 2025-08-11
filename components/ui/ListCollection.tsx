ee4f7bf6c1dd5f751a86f9e4cd242b197b3a7db8
ngoduy <21020293@vnu.edu.vn>
2025-08-11 17:27:44 +0700

    useCommonRepository

--- components/ui/ListCollection.tsx
+++ components/ui/ListCollection.tsx
@@ -2,28 +2,38 @@
 import { useEffect, useState } from "react";
 import CollectionCard from "./CollectionCard";
 import { getListCategories } from "@/services/productService";
+import useCommonRepository from "@/hooks/useCommonRepository";
 
 const ListCollection = () => {
   const [categories, setCategories] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getListCategories();
+  const {
+    records,
+    totalRecords,
+    fetching,
+    fetchRecords,
+    onParamsChange,
+    sortField,
+  } = useCommonRepository(getListCategories)
 
-        // Fake thêm dữ liệu bằng cách nhân bản
-        const fakeData = Array(4)
-          .fill(data)
-          .flat();
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const data = await getListCategories();
 
-        setCategories(fakeData);
-      } catch (error) {
-        console.error("Error fetching categories:", error);
-      }
-    };
+  //       // Fake thêm dữ liệu bằng cách nhân bản
+  //       const fakeData = Array(4)
+  //         .fill(data)
+  //         .flat();
 
-    fetchData();
-  }, []);
+  //       setCategories(fakeData);
+  //     } catch (error) {
+  //       console.error("Error fetching categories:", error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
 
   return (
     <div className="bg-[white] rounded-lg mt-[60px]">
