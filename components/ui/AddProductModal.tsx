9568480e0ee55b2c189580eb7c50ca973637a91a
Cao Thanh Trung <trung.cao10@mobifone.vn>
2025-08-13 14:46:09 +0700

    feat: upload multiple file

--- components/ui/AddProductModal.tsx
+++ components/ui/AddProductModal.tsx
@@ -42,7 +42,7 @@ const AddProductModal: React.FC<AddProductModalProps> = ({
       name: "",
       price: 0,
       quantity: 0,
-      image: null,
+      images: [],
       description: "",
     },
     resolver: zodResolver(ProductSchema),
@@ -86,7 +86,14 @@ const AddProductModal: React.FC<AddProductModalProps> = ({
         stockQuantity: data.quantity,
         categoryId: data.categoryId,
       };
-      await addProduct(payloadAddProduct, data.image);
+
+      console.log("payloadAddProduct", payloadAddProduct);
+
+      console.log("data.images", data.images);
+      
+      
+
+      await addProduct(payloadAddProduct, data.images);
       onSuccess?.();
       reset();
       Promise.resolve().then(() => {
@@ -180,15 +187,16 @@ const AddProductModal: React.FC<AddProductModalProps> = ({
         />
 
         <Controller
-          name="image"
+          name="images"
           control={control}
           render={({ field }) => (
             <BaseUpload
               id="image"
               {...field}
               label="Hình ảnh"
-              error={errors.image?.message}
-              onChange={(file) => setValue("image", file)}
+              error={errors.images?.message}
+              multiple={true}
+              onChange={(files: File[]) => field.onChange(files)}
             />
           )}
         />
--- services/productService.ts
+++ services/productService.ts
@@ -3,22 +3,26 @@ import api from "@/lib/axios";
 export const getListCategories = async () => {
   const response = await api.get("/category/all");
   return response.data;
-}
+};
 
 export const getAllProducts = async (params: any) => {
   const response = await api.get("/product/all", { params });
   return response.data;
 };
 
-export const addProduct = async (product: any, fileImage: any) => {
+export const addProduct = async (product: any, fileImages: File[]) => {
   const formData = new FormData();
-  formData.append("product", new Blob([JSON.stringify(product)], { type: "application/json" }));
-  formData.append("fileImage", fileImage)
+  formData.append(
+    "product",
+    new Blob([JSON.stringify(product)], { type: "application/json" })
+  );
+  fileImages.forEach((file) => {
+    formData.append("fileImage", file);
+  }); 
   const response = await api.post("/product/add", formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
-  }
-  );
+  });
   return response.data;
-}
+};
